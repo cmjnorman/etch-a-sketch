5e2272cb00132c9dbf5aa87db200f642b8570d69
cmjnorman <cmjnorman@gmail.com>
2020-02-10 17:01:47 +0000

    Added reset button.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,14 +1,24 @@
-let grid = document.getElementById("grid");
-for(i=0; i<16; i++) {
-    let row = document.createElement("div");
-    row.className = "row";
-    for(j=0; j<16; j++) {
-        let cell = document.createElement("div");
-        cell.className = "cell";
-        cell.addEventListener("mouseover", function(e) {
-            this.classList.add("activeCell");
-        });
-        row.appendChild(cell);
+function createGrid() {
+    let grid = document.getElementById("grid");
+    for(i=0; i<16; i++) {
+        let row = document.createElement("div");
+        row.className = "row";
+        for(j=0; j<16; j++) {
+            let cell = document.createElement("div");
+            cell.className = "cell";
+            cell.addEventListener("mouseover", function() {
+                this.classList.add("activeCell");
+            });
+            row.appendChild(cell);
+        }
+        grid.appendChild(row);
     }
-    grid.appendChild(row);
 } 
+
+let reset = document.getElementById("reset");
+reset.addEventListener("click", function() {
+    document.getElementById("grid").innerHTML = "";
+    createGrid();
+});
+
+createGrid()
